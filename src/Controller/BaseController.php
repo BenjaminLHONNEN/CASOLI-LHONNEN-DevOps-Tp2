01fd457e1784e4230f05843d9140815d378668fd
Bnj <benjaminlhonnen@gmail.com>
2018-11-20 08:30:24 -0800

    update nginx

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -4,14 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class BaseController extends AbstractController
 {
     /**
      * @Route("/", name="base")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->info('I just got the logger');
+        $logger->error('An error occurred');
+
+        $logger->critical('I left the oven on!', array(
+            // include extra "context" info in your logs
+            'cause' => 'in_hurry',
+        ));
         return $this->render('base/index.html.twig', [
             'controller_name' => 'BaseController',
         ]);
